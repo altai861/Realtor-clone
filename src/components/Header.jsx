b199d9f7f04af86f370e8c7bf9f8e763c1d55f2b
altai861 <altaizandanbileg@gmail.com>
2023-04-16 17:24:13 +0800

    Added signin, signup and forgot-password pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,9 +23,9 @@ const Header = () => {
             </div>
             <div>
                 <ul className="flex space-x-10">
-                    <li className={`cursor-pointer py-3 text-sm font-semi-bold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/") && "text-black border-b-red-500"}`} onClick={()=>navigate("/")}>Home</li>
-                    <li className={` cursor-pointer py-3 text-sm font-semi-bold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/offers") && "text-black border-b-red-500"}`} onClick={()=>navigate("/offers")}>Offers</li>
-                    <li className={` cursor-pointer py-3 text-sm font-semi-bold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/sign-in") && "text-black border-b-red-500"}`}onClick={()=>navigate("/sign-in")}>Sign in</li>
+                    <li className={`cursor-pointer py-3 text-sm font-semi-bold border-b-[3px] ${pathMatchRoute("/") ? "text-black border-b-red-500" :  "text-gray-400  border-b-transparent"}`} onClick={()=>navigate("/")}>Home</li>
+                    <li className={` cursor-pointer py-3 text-sm font-semi-bold  border-b-[3px] ${pathMatchRoute("/offers") ? "text-black border-b-red-500" :  "text-gray-400  border-b-transparent"} `} onClick={()=>navigate("/offers")}>Offers</li>
+                    <li className={` cursor-pointer py-3 text-sm font-semi-bold border-b-[3px] ${pathMatchRoute("/sign-in") ? "text-black border-b-red-500" :  "text-gray-400  border-b-transparent"}`} onClick={()=>navigate("/sign-in")}>Sign in</li>
                 </ul>
             </div>
         </header>
